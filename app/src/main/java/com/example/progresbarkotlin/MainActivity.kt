d19a87435c80d31a258aa98991154310c9bde1c6
marcinkolacz1976@gmail.com <marcinkolacz1976@gmail.com>
2022-02-28 13:25:09 +0100

    Pasek postępu

--- app/src/main/java/com/example/progresbarkotlin/MainActivity.kt
+++ app/src/main/java/com/example/progresbarkotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.progresbarkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<ProgressBar>(R.id.progressBar).incrementProgressBy(1)
+            findViewById<ProgressBar>(R.id.progressBar2).incrementProgressBy(2)
+        }
     }
 }
