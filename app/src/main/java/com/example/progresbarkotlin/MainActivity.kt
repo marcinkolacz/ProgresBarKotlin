d49fad535150781639c8ee1b8ef9384f4f9885a1
marcinkolacz1976@gmail.com <marcinkolacz1976@gmail.com>
2022-03-07 16:14:33 +0100

    dodany gradient shape

--- app/src/main/java/com/example/progresbarkotlin/MainActivity.kt
+++ app/src/main/java/com/example/progresbarkotlin/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.progresbarkotlin
 
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.ScaleDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button).setOnClickListener {
             findViewById<ProgressBar>(R.id.progressBar).incrementProgressBy(1)
             findViewById<ProgressBar>(R.id.progressBar2).incrementProgressBy(2)
+
+            val shape: Drawable? = getDrawable(R.drawable.gradient_box)
+            val tv: TextView = findViewById(R.id.textView)
+            tv.background = shape
         }
     }
 }
